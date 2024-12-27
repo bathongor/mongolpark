1c6dadb2d0a0f9c3e5932191064eb2a774ff1185
bathongor <ch.bathongor@gmail.com>
2024-12-27 23:51:56 +0100

    page.tsx fixed

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,7 +2,5 @@ import { add as addNumbers } from '@mongolpark/sample-lib'
 import React from 'react'
 
 export default function Home() {
-  const a = 0
-  a = ''
   return <div>{addNumbers(4, 6)}</div>
 }
