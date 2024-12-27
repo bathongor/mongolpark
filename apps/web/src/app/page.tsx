4577df22c2b5b8e7f2a13817a68292a6c3b208a3
bathongor <ch.bathongor@gmail.com>
2024-12-27 23:47:43 +0100

    [build/added] husky working

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,5 +2,7 @@ import { add as addNumbers } from '@mongolpark/sample-lib'
 import React from 'react'
 
 export default function Home() {
+  const a = 0
+  a = ''
   return <div>{addNumbers(4, 6)}</div>
 }
